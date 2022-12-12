e1b501267ae4dc5ca6e15c2939be6587793b6101
Adewalw101 <elijah2ade@yahoo.co.uk>
2022-12-12 14:26:39 -0800

    Hello, Word

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - entry point
+ *Description: print if number is poositive or negative
+ *Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand (time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>0)
+	
+	
+		printf("%d is positiven\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else 
+	{
+		print("%d is negative\n", n);
+	}
+	retun (0);
+}
