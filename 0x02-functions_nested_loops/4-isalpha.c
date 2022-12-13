f9cb2c3fec1eda1cfb82db11c12980942c9d7c29
Adewalw101 <elijah2ade@yahoo.co.uk>
2022-12-13 12:08:30 -0800

    putchar

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,10 +4,10 @@
  * @c: character
  * return: 1 if c is a letter, lowercase or uppercase Returns 0 otherwise
  */
-int __isalpha(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
-		return
+		return (0)
 }
