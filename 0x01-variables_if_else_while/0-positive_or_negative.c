3bbf613a7f9e7a16779fd698c541d4a65d2d2110
Adewalw101 <elijah2ade@yahoo.co.uk>
2022-12-12 19:42:57 -0800

    Hello, Word

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,26 +4,27 @@
 /**
  *main - entry point
  *Description: print if number is poositive or negative
- *Return: 0
+ *Return: Always 0
  */
 int main(void)
 {
 	int n;
 
-	srand (time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
-	
-	
-		printf("%d is positiven\n", n);
+
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+	printf("%d is zero\n", n);
 	}
 	else 
 	{
-		print("%d is negative\n", n);
+	printf("%d is negative\n", n);
 	}
+
 	retun (0);
 }
