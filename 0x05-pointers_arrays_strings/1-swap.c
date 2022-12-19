8d0ee75fd92e45b6143376289522c9c60b3769e7
Adewalw101 <elijah2ade@yahoo.co.uk>
2022-12-19 13:49:00 -0800

    lets run some test arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* swap_int - swaps the value of int d and int g
+* Omo E no easy ooooo
+* @a: first int
+* @b: second int
+* Return 0
+*/
+void swap_int(int *a, int *b)
+{
+		int f;
+
+		f = *a;
+		*a = *b;
+		*b = f;
+}
