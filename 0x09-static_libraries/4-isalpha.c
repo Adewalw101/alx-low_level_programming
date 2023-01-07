9c4a0ddec27d683992f6b93c7d6c09cec4cf1ac7
Adewalw101 <elijah2ade@yahoo.co.uk>
2023-01-07 03:13:33 -0800

    First shege for alx

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_isalpha - this is a program to print out alphabets
+ *@c: character
+ *Return: 1 if c is a letter, lowercase or uppercase, or 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
